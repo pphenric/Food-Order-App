350ad285a6b02998429a97bb5979b91962a6a567
Iliesi Paul <i.paulnick@yahoo.com>
2025-03-28 16:58:26 +0200

    bug fixing

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
     ],
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL, // Expose DATABASE_URL
   },
 };
 
